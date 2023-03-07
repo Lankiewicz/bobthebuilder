61b8a5c4c1e806a784c287730e4ac84ece47f60f
Jakub <lankiewiczjakub@gmail.com>
2023-03-07 10:27:50 +0100

    Adding login endpoint

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const db = require('../models/db');
 const signup = require('../API/signUp');
+const login = require('../API/login');
+
+
+app.use('/login', login);
+app.use('/signup', signup);
+app.use('/', signup);
 
 app.use(express.json());
 
-app.use('/', signup);
-
 // Konfiguracja i uruchomienie serwera
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3007;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
