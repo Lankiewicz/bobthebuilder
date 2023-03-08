9b1fc296e67b8cddf50f17df15c1d606eaf506b4
Jakub <lankiewiczjakub@gmail.com>
2023-03-08 11:18:42 +0100

    adding token JWT to auth process

--- src/app.js
+++ src/app.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const db = require('../models/db');
 const signup = require('../API/signUp');
-const login = require('../API/login');
+const getAuthRouter = require('../API/login');
+const getUsersRouter = require('../API/getUsers');
 
 
-app.use('/login', login);
+app.use(getAuthRouter);
 app.use('/signup', signup);
 app.use('/', signup);
+app.use(getUsersRouter);
 
 app.use(express.json());
 
 // Konfiguracja i uruchomienie serwera
-const port = process.env.PORT || 3007;
+const port = process.env.PORT || 3019;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
